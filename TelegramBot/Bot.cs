4a903df5f8c9c0f7c0ccd76c38e296a6f1c0f35d
sokelva <sokelva@mail.ru>
2023-01-30 13:33:41 +0300

    Задание 7

--- TelegramBot/Bot.cs
+++ TelegramBot/Bot.cs
@@ -1,42 +1,46 @@
-﻿using System;
+﻿//using System;
+//using System.Threading.Tasks;
 
-namespace TelegramBot
-{
-    class Bot
-    {
+//namespace TelegramBot
+//{
+//    class Bot
+//    {
 
-        /// <summary>
-        /// объект, отвечающий за отправку сообщений клиенту
-        /// </summary>
-        private TelegramBotClient _telegramClient;
+//        /// <summary>
+//        /// объект, отвечающий за отправку сообщений клиенту
+//        /// </summary>
+//        private IBotClient _telegramClient;
 
-        public Bot(TelegramBotClient telegramClient)
-        {
-            _telegramClient = telegramClient;
-        }
+//        public Bot(IBotClient telegramClient)
+//        {
+//            _telegramClient = telegramClient;
+//        }
 
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            _telegramClient.OnMessage += HandleMessage;
-            _telegramClient.OnMessage += HandleButtonClick;
+//        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+//        {
+//            _telegramClient.OnMessage += HandleMessage;
+//            _telegramClient.OnMessage += HandleButtonClick;
 
-            Console.WriteLine("Bot started");
-        }
+//            Console.WriteLine("Bot started");
+//        }
 
-        /// <summary>
-        /// Обработчик входящих текстовых сообщений  
-        /// </summary>
-        private async Task HandleMessage(object sender, MessageEventArgs e)
-        {
+//        /// Обработчик входящих текстовых сообщений  
+//        /// </summary>
+//        private async Task HandleMessage(object sender, MessageEventArgs e)
+//        {
+//            // Бот получил входящее сообщение пользователя
+//            var messageText = e.Message.Text;
 
-        }
+//            // Бот Отправляет ответ
+//            _telegramClient.SendTextMessage(e.ChatId, "Ответ на сообщение пользователя")
+//        }
 
-        /// <summary>
-        /// Обработчик нажатий на кнопки
-        /// </summary>
-        private async Task HandleButtonClick(object sender, MessageEventArgs e)
-        {
+//        /// <summary>
+//        /// Обработчик нажатий на кнопки
+//        /// </summary>
+//        private async Task HandleButtonClick(object sender, MessageEventArgs e)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TelegramBot
+namespace Example
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Vehicle transport = new Truck(); // реально создали объект Truck, но забыли переопределить метод Move в Truck
+            transport.Move();
+            Console.ReadKey();
+        }
+    }
+
+    public class Vehicle
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("Moving");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Car moving");
+        }
+    }
+
+    public class Truck : Car
+    {
+        public void Move() // забыли переопределить метод Move
+        {
+            Console.WriteLine("Truck moving");
         }
     }
 }
