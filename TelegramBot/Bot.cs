f0a8eede8b6e38a4eafb1fe8561cf62c85a3db5a
sokelva <sokelva@mail.ru>
2023-01-30 11:55:21 +0300

    Добавлен класс Bot.cs

---
+++ TelegramBot/Bot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class Bot
+    {
+
+        /// <summary>
+        /// объект, отвечающий за отправку сообщений клиенту
+        /// </summary>
+        private TelegramBotClient _telegramClient;
+
+        public Bot(TelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            _telegramClient.OnMessage += HandleMessage;
+            _telegramClient.OnMessage += HandleButtonClick;
+
+            Console.WriteLine("Bot started");
+        }
+
+        /// <summary>
+        /// Обработчик входящих текстовых сообщений  
+        /// </summary>
+        private async Task HandleMessage(object sender, MessageEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Обработчик нажатий на кнопки
+        /// </summary>
+        private async Task HandleButtonClick(object sender, MessageEventArgs e)
+        {
+
+        }
+    }
+}
