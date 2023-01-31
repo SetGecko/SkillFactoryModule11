291a345b8d7f9d8465afdc3aaf53981f23b5fac3
sokelva <sokelva@mail.ru>
2023-01-31 14:16:50 +0300

    Подключение библиотек TelegramBot.

---
+++ TelegramBot_Net6.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramBot_Net6._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
