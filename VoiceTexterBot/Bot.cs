6e7fef0c12185e20eb78bf6818a63e6af033e8e3
sokelva <sokelva@mail.ru>
2023-02-01 17:19:20 +0300

    Доавлен класс bot и метод async Task HandleUpdateAsync.

--- VoiceTexterBot/Bot.cs
+++ VoiceTexterBot/Bot.cs
@@ -15,5 +15,22 @@ namespace VoiceTexterBot
             _telegramClient = telegramClient;
             //Telegram.Bot.Exceptions.;
         }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            //  Обрабатываем нажатия на кнопки  из Telegram Bot API: https://core.telegram.org/bots/api#callbackquery
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
+                return;
+            }
+
+            // Обрабатываем входящие сообщения из Telegram Bot API: https://core.telegram.org/bots/api#message
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы отправили сообщение", cancellationToken: cancellationToken);
+                return;
+            }
+        }
     }
 }
