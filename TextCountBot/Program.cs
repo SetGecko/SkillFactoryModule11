5a369db7d20edc441bf75d2a40f6c3fa77f81a71
sokelva <sokelva@mail.ru>
2023-02-07 17:50:28 +0300

    Добавлен новый функционал для нового бота.

--- TextCountBot/Program.cs
+++ TextCountBot/Program.cs
@@ -4,29 +4,55 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
+using TextCountBot;
+using VoiceTexterBot.Configuration;
+using VoiceTexterBot.Controllers;
+using VoiceTexterBot.Services;
 
-namespace TextCountBot
+namespace VoiceTexterBot
 {
-    static class Program
+    public class Program
     {
         public static async Task Main()
         {
             Console.OutputEncoding = Encoding.Unicode;
 
+            // Объект, отвечающий за постоянный жизненный цикл приложения
             var host = new HostBuilder()
-                .ConfigureServices((hostContext, services) => ConfigureServices(services))
-                .UseConsoleLifetime()
-                .Build();
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
 
-            Console.WriteLine("Starting Service");
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
             await host.RunAsync();
-            Console.WriteLine("Service stopped");
+            Console.WriteLine("Сервис остановлен");
         }
 
         static void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6095931565:AAG4iDTNwhC4tvyOTdIZfu6DATMepe-Mm8E"));
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Подключаем контроллеры сообщений и кнопок
+            //services.AddTransient<DefaultMessageController>();
+            //services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+            //services.AddSingleton<IFileHandler, AudioFileHandler>();
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(appSettings.BotToken));
             services.AddHostedService<Bot>();
+        }
+
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                DownloadsFolder = "C:\\Users\\sokelva",
+                BotToken = "6095931565:AAG4iDTNwhC4tvyOTdIZfu6DATMepe-Mm8E" //SokelvaNewBot
+            };
         }
     }
 }
